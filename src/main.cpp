5ce9eb8d3dc78b7c9de1179fe3e4039eb68fb1fc
whattopick <37048109+whattopick@users.noreply.github.com>
2018-03-23 15:32:50 +0200

    updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,6 @@ using namespace std;
 
 int main()
 {
-    //printf("********************************************************** CRAZY TANKS ************************************************\n");
     ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
     printf("Input width of field (min 20, max 210): ");
     int width;
@@ -34,32 +33,25 @@ int main()
     cursorController->drawAtPlace(0, 1, "                                          ");
     cursorController->drawAtPlace(0, 1, "Loading, please wait");
     cursorController->isFree_=true;
-//    UserInterface * userInterface = new UserInterface(cursorController);
     Field * field = new Field(width, height, cursorController);
     while(cursorController->isFree_==false);
     cursorController->isFree_=false;
     cursorController->drawAtPlace(0, 1, "                                          ");
     cursorController->isFree_=true;
-    //userInterface->drawField(field->getFieldInfo().x, field->getFieldInfo().y, field->getFieldInfo().width, field->getFieldInfo().height, field->level_);
     AllyTank * allyTank = new AllyTank(field, cursorController);
     EnemyTank * enemyTanks[field->getEnemiesCount()];
     srand(time(NULL));
     for (int i=0; i<field->getEnemiesCount(); i++){
     EnemyTank * enemyTank = new EnemyTank(field, rand()%(field->getFieldInfo().width-3)+1,rand()%(field->getFieldInfo().height-3)+1, cursorController, allyTank);
     enemyTanks[i]=enemyTank;
-    //Sleep(20);
     }
-    //GameTimer * timer = new GameTimer(userInterface);
     GameTimer * timer = new GameTimer(cursorController);
     std::thread t1([timer]{timer->tick();});
     std::thread t2([allyTank]{allyTank->controlTank();});
-    //std::thread t3([allyTank]{allyTank->shoot();});
     t1.join();
     delete timer;
-   // delete enemyTank;
     delete allyTank;
     delete field;
-    //delete userInterface;
     delete cursorController;
     return 0;
 }
